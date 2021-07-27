647bd3d082fdffbaed718cb66e7ae7ef8f929244
akshay <akshaydamare09@gmail.com>
2021-07-27 10:11:03 +0530

    Add Hello world

---
+++ Hello.java/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        
+    }
+}
+//    git remote add origin https://github.com/akshayd22/JavaPractice.git
+//        git branch -M main
+//        git push -u origin main
